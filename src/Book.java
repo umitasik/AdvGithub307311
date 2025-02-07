220f5b7f7a145ea28a3df8dcbebff55da6116244
Ümit AŞIK <umitasikumit@gmail.com>
2025-02-07 20:47:29 +0300

    version2

---
+++ src/Book.java
@@ -0,0 +1,6 @@
+public class Book {
+
+    //Değişiklik 2
+    public String name;
+
+}
--- src/MyClass.java
+++ src/MyClass.java
@@ -2,5 +2,8 @@ public class MyClass {
     public static void main(String[] args) {
         //değişiklik
         System.out.println("Herkese Selamlar...");
+
+        //değişiklik 2
+        System.out.println("Adv Github Dersi...");
     }
 }
